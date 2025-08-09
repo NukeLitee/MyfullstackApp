a82d69ea85337234b7d6e7d5bae1cd39a37b285b
NukeLitee <haodeng17@gmail.com>
2025-08-09 18:22:49 +0700

    09/8

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // URL gốc của API
+    baseURL: `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api', // URL gốc của API
 });
 
 // Sử dụng interceptor để tự động thêm token vào mỗi request
